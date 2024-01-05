b76abc32129bdfcfd1751bc00157409f34a9ad62
Shibli2316 <84792951+Shibli2316@users.noreply.github.com>
2024-01-05 21:13:50 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count pairs with given sum/count-pairs-with-given-sum.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+int getPairsCount(int arr[], int n, int k) {
+    int answer = 0;
+    std::unordered_map<int, int> frequency;
+    for (int i = 0; i < n; i++) {
+        int complement = k - arr[i];
+        if (frequency.find(complement) != frequency.end()) {
+            answer += frequency[complement];
+        }
+        frequency[arr[i]]++;
+    }
+    return answer;
+}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.getPairsCount(arr, n, k);
+        cout << ans << "\n";
+    }
+    
+    return 0;
+}
+// } Driver Code Ends
